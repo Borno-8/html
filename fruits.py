8c5c4127874a9cb1bab2ec54623072b5d49985bd
Borno-8 <rasel_bt_ku@yahoo.com>
2026-04-04 22:32:37 +0600

    Add files via upload

---
+++ fruits.py
@@ -0,0 +1,21 @@
+lst = ['apple','mango','guava','kiwi','watermelon']
+print("Lenght of list:",len(lst))
+print("First Element:",lst[0])
+print("last Element", lst[-1])
+
+
+
+lst.appened('Papaya')
+print("Updated List :", lst)
+
+lst.remove('Mango')
+print("Updated List :", lst)
+
+lst.appened('Papaya')
+print("Updated List :", lst)
+
+lst.appened('Papaya')
+print("Updated List :", lst)
+
+lst.appened('Papaya')
+print("Updated List :", lst)
