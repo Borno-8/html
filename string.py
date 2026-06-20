1811100eee761c53300ad9e8ec2c5dba62daadf4
Borno-8 <rasel_bt_ku@yahoo.com>
2026-06-20 22:14:16 +0600

    Add files via upload

---
+++ string.py
@@ -0,0 +1,7 @@
+string = input("please enter ur own string!!!😁")
+string2 = ('')
+for i in string:
+    string2 = i + string2
+
+print("\nDa original string was🤔 = ", string)
+print("Soooo...I know what the reversed string is😎😏! its :", string2)
